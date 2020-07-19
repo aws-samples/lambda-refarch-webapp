3c25d0dafad10e73894d2c821f65d44ffd17d081
Mike Apted <mike.apted@gmail.com>
2020-07-19 12:05:10 -0400

    Udpates to deployment script and config files. Closes #35

--- www/src/config.js
+++ www/src/config.js
@@ -2,10 +2,10 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "2t6ql38oq0g12581d4tuq1vuo5",     // CognitoClientID
-  "api_base_url": "https://i116m3u68d.execute-api.us-east-1.amazonaws.com/prod",                                     // TodoFunctionApi
-  "coginto_hosted_domain": "mytodoappdemo-lambda-issue-35.auth.us-east-1.amazoncognito.com",                   // CognitoDomainName
-  "redirect_url": "https://issue-35.d26y510u49hqga.amplifyapp.com"                                      // AmplifyURL
+  "aws_user_pools_web_client_id": "None",     // CognitoClientID
+  "api_base_url": "None",                                     // TodoFunctionApi
+  "coginto_hosted_domain": "None",                   // CognitoDomainName
+  "redirect_url": "None"                                      // AmplifyURL
 };
 
 export default config;
