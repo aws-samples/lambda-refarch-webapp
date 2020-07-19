b0a003f00c562d5437f944abf65388fa95e8e6e5
Mike Apted <mike.apted@gmail.com>
2020-07-19 11:44:18 -0400

    Config update

--- www/src/config.js
+++ www/src/config.js
@@ -5,7 +5,7 @@ const config = {
   "aws_user_pools_web_client_id": "5ike2fb50ibl0k0qff5sr5c3ja",     // CognitoClientID
   "api_base_url": "https://s7yujxxc15.execute-api.us-east-1.amazonaws.com/prod",                                     // TodoFunctionApi
   "coginto_hosted_domain": "mytodoappdemo-lambda-issue-35.auth.us-east-1.amazoncognito.com",                   // CognitoDomainName
-  "redirect_url": "https://amplify-console.d1el6ol1w70dql.amplifyapp.com"                                      // AmplifyURL
+  "redirect_url": "https://amplify-console.d1el6ol1w70dql.amplifyapp.com"                                      // AmplifyURL 
 };
 
 export default config;
