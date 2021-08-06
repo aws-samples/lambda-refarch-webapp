7f4b06a9fe01b3ce3691d05ccc6c71f69c46fc0f
Michael Stanfa <stanfa.michael@gmail.com>
2021-08-05 21:59:45 -0500

    adding prod

--- www/src/config.js
+++ www/src/config.js
@@ -3,7 +3,7 @@
 
 const config = {
   "aws_user_pools_web_client_id": "1nikeekmd9cq5d5760ugquv0o5",     // CognitoClientID
-  "api_base_url": "https://qtqhkw00n4.execute-api.us-east-1.amazonaws.com/{StageNameParam}",                                     // TodoFunctionApi
+  "api_base_url": "https://qtqhkw00n4.execute-api.us-east-1.amazonaws.com/prod",                                     // TodoFunctionApi
   "cognito_hosted_domain": "mytodoappdemo-example-stack.auth.us-east-1.amazoncognito.com",                   // CognitoDomainName
   "redirect_url": "https://master.d32ym3po20wpfu.amplifyapp.com"                                      // AmplifyURL
 };
