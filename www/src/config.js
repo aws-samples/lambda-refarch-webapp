06064e6264beab0842cb45d806bacf4031c113f0
GQMai <gq.mai@aliyun.com>
2021-10-06 16:33:18 +0800

    Frontend config update

--- www/src/config.js
+++ www/src/config.js
@@ -2,10 +2,10 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "None",     // CognitoClientID
-  "api_base_url": "None",                                     // TodoFunctionApi
-  "cognito_hosted_domain": "None",                   // CognitoDomainName
-  "redirect_url": "None"                                      // AmplifyURL
+  "aws_user_pools_web_client_id": "2jpqog2ak1t3qt6fmsrmrnrdbo",     // CognitoClientID
+  "api_base_url": "https://1d4ldfwec0.execute-api.ap-southeast-1.amazonaws.com/dev",                                    // TodoFunctionApi
+  "cognito_hosted_domain": "mytodoappdemo-todo-demo.auth.ap-southeast-1.amazoncognito.com",                   // CognitoDomainName
+  "redirect_url": "https://master.d1ooex4fqfbp8g.amplifyapp.com"                                    // AmplifyURL
 };
 
 export default config;
